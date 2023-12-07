cb2b0317fc16d79fd17adc5f62cb66636347874c
rose <rose@outlook.com>
2023-12-07 20:44:12 +0800

    第二次更新Demo1文件

--- src/main/java/com/devil/revenge/Demo1.java
+++ src/main/java/com/devil/revenge/Demo1.java
@@ -3,5 +3,6 @@ package com.devil.revenge;
 public class Demo1 {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello bess");
     }
 }
