d006fcc6eb8446d67f8d3eb8ac0670b86a5af93d
rose <rose@outlook.com>
2023-12-07 20:46:20 +0800

    第三次更新Demo1文件

--- src/main/java/com/devil/revenge/Demo1.java
+++ src/main/java/com/devil/revenge/Demo1.java
@@ -4,5 +4,6 @@ public class Demo1 {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello bess");
+        System.out.println("hello Cara");
     }
 }
