0c1b1223d4c59e565f34261869a9f7c2f9e11051
rose <rose@outlook.com>
2023-12-08 00:43:18 +0800

    更新Demo2文件第一次

--- src/main/java/com/devil/revenge/Demo2.java
+++ src/main/java/com/devil/revenge/Demo2.java
@@ -3,6 +3,7 @@ package com.devil.revenge;
 public class Demo2 {
     public static void main(String[] args) {
         System.out.println("revenge mango one");
+        System.out.println("revenge mango two");
     }
 }
 
