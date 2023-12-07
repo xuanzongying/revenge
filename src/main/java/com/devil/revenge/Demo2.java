490f2f21aa3485c831491b6d1d44b500caab4605
xuanzongying <99346985+xuanzongying@users.noreply.github.com>
2023-12-08 00:44:18 +0800

    GitHub上更新Update Demo2.java

--- src/main/java/com/devil/revenge/Demo2.java
+++ src/main/java/com/devil/revenge/Demo2.java
@@ -4,6 +4,7 @@ public class Demo2 {
     public static void main(String[] args) {
         System.out.println("revenge mango one");
         System.out.println("revenge mango two");
+        System.out.println("revenge mango three");
     }
 }
 
